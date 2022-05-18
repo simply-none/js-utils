3cd358c0802b6c54c772dee607368db0b2406714
jousindea <jousindea@163.com>
2022-05-18 22:27:02 +0800

    增加提示（使用ts）

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export declare function numToWord(num: any): any;
+export declare function wordToNum(word: any): any;
+declare function uniqueArray(arr: any, key: any): any;
+declare const _default: {
+    uniqueArray: typeof uniqueArray;
+};
+export default _default;
